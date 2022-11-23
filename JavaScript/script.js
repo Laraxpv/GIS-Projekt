057cdc70aaf9d16dc587ac3de4dd72cfd029d19c
Laraxpv <larapav@web.de>
2022-11-23 12:49:52 +0100

    Inputs geladen in JavaScript und mit dem submitbutton angefangen

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,4 +1,27 @@
 let p = document.createElement('a'); // neues a Element
 p.textContent = 'Alles mit DOM und JavaScript erstellt.';
+p.href="Pflanzendetail.html";
+p.className="tablinks";
+
+
 let body = document.body;
 body.appendChild(p);
+
+
+function submit(){let add = document.getElementById("hinzufügen"); 
+
+let name = document.getElementById("name").value;
+
+let klima = document.getElementById("klima").value;
+
+let giesseinheit = document.getElementById("giesseinheit").value;
+
+const diePflanze = {name:name , klima:klima , giesseinheit:giesseinheit}
+
+} 
+
+let submitbutton = document.getElementById("submitbutton");
+
+submitbutton.addEventListener("click", submit);
+
+let pflanzenliste = []; 
